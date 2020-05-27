c1002ea0fbc0a9248ee0c5e52d26cb5c6f5a789f
Gugunner <ram2489@gmail.com>
2020-05-27 18:02:33 -0500

    Finished scatter plot

--- src/index.js
+++ src/index.js
@@ -7,94 +7,171 @@ document.getElementById("app").innerHTML = `
 
 </div>
 `;
+
 const chartWidth = 920;
 const chartHeight = 630;
-const padding = 60;
-const sortedDataset = _.sortBy(dataset, d => d["Year"]);
+const padding = 130;
+const sortedDataset = _.sortBy(dataset, ({ Year }) => Year);
 
-const minDomainXValue = d3.min(sortedDataset, d => d["Year"]);
-const maxDomainXValue = d3.max(sortedDataset, d => d["Year"] + 1);
-const minDomainYValue = d3.min(sortedDataset, d =>
-  Number(d["Time"].replace(/:/gi, "."))
-);
-const maxDomainYValue = d3.max(sortedDataset, d =>
-  Number(d["Time"].replace(/:/gi, "."))
-);
+const timeParser = d3.timeParse("%M:%S");
+const timeFormatter = d3.timeFormat("%M:%S");
+const xAxisValues = [
+  ..._.map(_.sortBy(_.uniqBy(dataset, "Year"), "Year"), ({ Year }) => Year),
+  2016
+];
+
+const minDomainYValue = timeParser(d3.min(dataset, ({ Time }) => Time));
+const maxDomainYValue = timeParser(d3.max(dataset, ({ Time }) => Time));
+console.log(minDomainYValue);
 
 const xScale = d3
-  .scaleLinear()
-  .domain([minDomainXValue, maxDomainXValue])
+  .scaleBand()
+  .domain(xAxisValues)
   .range([padding, chartWidth - padding]);
 
 const yScale = d3
-  .scaleLinear()
-  .domain([maxDomainYValue, minDomainYValue])
+  .scaleTime()
+  .domain([
+    d3.timeSecond.offset(maxDomainYValue, 5),
+    d3.timeSecond.offset(minDomainYValue, -5)
+  ])
   .range([chartHeight - padding, padding]);
 
-console.log("Min Domain X Value", minDomainXValue);
-console.log("Max Domain X Value", maxDomainXValue);
-console.log("Min Domain Y Value", minDomainYValue);
-console.log("Max Domain Y Value", maxDomainYValue);
-
-console.log("Max XScale", xScale(maxDomainXValue));
-console.log("Min XScale", xScale(minDomainXValue));
-console.log("Max YScale", yScale(maxDomainYValue));
-console.log("Min YScale", yScale(minDomainYValue));
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3
+  .axisLeft(yScale)
+  .ticks(10)
+  .tickFormat(timeFormatter);
 
 const svg = d3
   .select("#chart")
   .append("svg")
-  .attr("width", chartWidth + padding)
-  .attr("height", chartHeight);
+  .attr("width", chartWidth)
+  .attr("height", chartHeight)
+  .attr("class", "graph")
+  .append("g")
+  .attr("transform", `translate(60,100)`);
 
-const title = svg.append("text");
-title
-  .attr("id", "title")
-  .attr("x", chartHeight / 4)
-  .attr("y", padding + 10)
-  .text("Doping in Professional Bycicle Racing");
+const legend = d3
+  .select("#app")
+  .select("#chart")
+  .attr("position", "absolute")
+  .style("width", "950px")
+  .insert("div")
+  .attr("id", "legend")
+  .style("display", "block")
+  .style("position", "relative")
+  .style("left", `${70}%`)
+  .style("top", `-${chartHeight / 2}px`);
 
-let minute = -1;
-let seconds = 0;
-const timeSpecifier = "%M:%S";
-const yLabels = _.map(
-  _.map(_.fill(Array(12), 37), (time, idx) => {
-    seconds += 15;
-    if (idx === 0 || seconds === 60) {
-      minute++;
-      seconds = 0;
-    }
-    const stringSeconds = `${seconds === 0 ? "00" : seconds}`;
-    return `${time + minute}:${stringSeconds}`;
-  }),
-  time => d3.timeParse(timeSpecifier)(time)
-);
-console.log("Y Labels", yLabels);
+legend
+  .insert("div")
+  .attr("id", "legend-a")
+  .style("display", "flex")
+  .style("justify-content", "flex-end")
+  .style("align-items", "center")
+  .insert("p")
+  .style("font-weight", "700")
+  .style("margin", "5px")
+  .text("No doping allegations");
+// LegendA Circle
+d3.select("#legend-a")
+  .insert("div")
+  .attr("id", "legend-a-circle")
+  .style("width", "15px")
+  .style("height", "15px")
+  .style("border-radius", "50%")
+  .style("background-color", "rgb(255, 127, 14)");
 
-const xAxis = d3.axisBottom(xScale);
-const timeScale = d3
-  .scaleTime()
-  .domain(_.reverse(d3.extent(yLabels)))
-  .nice()
-  .range([chartHeight - padding, padding]);
-
-const yAxis = d3.axisLeft(timeScale);
-
-svg
-  .selectAll("circle")
-  .data(dataset)
-  .append("circle");
+legend
+  .insert("div")
+  .attr("id", "legend-b")
+  .style("display", "flex")
+  .style("justify-content", "flex-end")
+  .style("align-items", "center")
+  .insert("p")
+  .style("font-weight", "700")
+  .style("margin", "5px")
+  .text("Riders with doping allegations");
+// LegendB Circle
+d3.select("#legend-b")
+  .insert("div")
+  .attr("id", "legend-a-circle")
+  .style("width", "15px")
+  .style("height", "15px")
+  .style("border-radius", "50%")
+  .style("background-color", "rgb(31,119,180)");
 
 svg
   .append("g")
   .attr("id", "x-axis")
   .attr("transform", `translate(0,${chartHeight - padding})`)
-  .call(xAxis.tickFormat(d => d));
+  .call(xAxis);
 
 svg
   .append("g")
   .attr("id", "y-axis")
   .attr("transform", `translate(${padding},${0})`)
-  .call(
-    yAxis.tickValues(yLabels).tickFormat(d => d3.timeFormat(timeSpecifier)(d))
-  );
+  .call(yAxis);
+
+const title = svg.append("text");
+title
+  .attr("id", "title")
+  .attr("x", chartHeight / 2 - padding)
+  .attr("y", 60)
+  .text("Doping in Professional Bycicle Racing");
+
+const subtitle = svg.append("text");
+subtitle
+  .attr("id", "subtitle")
+  .attr("x", chartHeight / 2 - padding / 2)
+  .attr("y", 90)
+  .text("35 Fastest times up Alpe d'Huez");
+
+const yLabel = svg.append("text");
+yLabel
+  .attr("id", "y-label")
+  .attr("x", -chartHeight / 2)
+  .attr("y", padding / 2)
+  .text("Time in Minutes")
+  .style("transform", "rotate(-90deg)");
+
+svg
+  .selectAll("circle")
+  .data(sortedDataset)
+  .enter()
+  .append("circle")
+  .attr("class", "dot")
+  .style("fill", ({ Doping }) =>
+    !Doping ? "rgb(255, 127, 14)" : "rgb(31,119,180)"
+  )
+  .style("cursor", "pointer")
+  .attr("data-xvalue", ({ Year }) => xScale(Year))
+  .attr("data-yvalue", ({ Time }) => yScale(timeParser(Time)))
+  .attr("cx", ({ Year }) => xScale(Year) + xScale.bandwidth() / 2)
+  .attr("cy", ({ Time }) => yScale(timeParser(Time)))
+  .attr("r", 5)
+  .on("mouseover", ({ Name, Nationality, Year, Time, Place, Doping }) => {
+    tooltip
+      .attr("data-year", () => xScale(Year))
+      .style("display", "block")
+      .style("position", "absolute")
+      .style("left", () => `${xScale(Year) + xScale.bandwidth() / 2}px`)
+      .style("top", () => `${yScale(timeParser(Time))}px`)
+      .style("transform", "translate(90px, 90px)")
+      .html(
+        `<p>${Name} - <span>${Nationality}</span></p>
+      <hr>
+      <p>Year: ${Year}</p>
+      <p>Place: ${Place}</p>
+      <p>TIme: ${Time}</p>
+      ${Doping === "" ? "" : `<hr><p>${Doping}</p>`}`
+      );
+  })
+  .on("mouseleave", () => tooltip.style("display", "none"));
+
+const tooltip = d3
+  .select("#chart")
+  .insert("div")
+  .attr("id", "tooltip")
+  .style("display", "none");
